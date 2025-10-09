370bdf1849bc9e2370a2c880c86d5463d0b89273
Facudevida <facudevida5@gmail.com>
2025-10-08 21:58:30 -0300

    Agrego autorizacion a routes de producto

--- backend/src/routes/producto.routes.ts
+++ backend/src/routes/producto.routes.ts
@@ -1,17 +1,17 @@
 import { Router } from "express";
 import * as productoController from "../controllers/producto.controller";
 import { validate } from "../middlewares/validation.middleware";
-import {
-  crearProductoSchema,
-  actualizarProductoSchema,
-} from "../validations/producto.validation";
+import { crearProductoSchema, actualizarProductoSchema } from "../validations/producto.validation";
+import { authenticate, authorize } from '../middlewares/auth.middleware';
 
 const router = Router();
 
-router.get("/", productoController.getAllProducts);
-router.get("/:id", productoController.getProductById);
-router.post("/", validate(crearProductoSchema), productoController.createProduct);
-router.put("/:id", validate(actualizarProductoSchema), productoController.updateProduct);
-router.delete("/:id", productoController.deleteProduct);
+router.use(authenticate);
+
+router.get("/",authorize('USER', 'ADMIN'), productoController.getAllProducts);
+router.get("/:id",authorize('USER', 'ADMIN'), productoController.getProductById);
+router.post("/",authorize('ADMIN'), validate(crearProductoSchema), productoController.createProduct);
+router.put("/:id",authorize('ADMIN'), validate(actualizarProductoSchema), productoController.updateProduct);
+router.delete("/:id",authorize('ADMIN'), productoController.deleteProduct);
 
 export const productoRoutes = router;
