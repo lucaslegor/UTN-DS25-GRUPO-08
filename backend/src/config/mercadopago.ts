49b8ab1a783793dcfa965ff8dbd493b2489caa35
Joacorodriguezz <joaco.rodriguez800@gmail.com>
2025-10-28 20:30:51 -0300

    rm mp.config

--- backend/src/config/mercadopago.ts
+++
@@ -1,8 +0,0 @@
-import mercadopago from "mercadopago";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-mercadopago.configurations.setAccessToken(process.env.MERCADOPAGO_ACCESS_TOKEN);
-
-export default mercadopago;
